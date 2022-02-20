e2dc1073fd9b7b4b1b25b37b815d25ad9fbbd755
ally-petitt <allypetitt@gmail.com>
2022-02-20 15:05:42 -0800

    added template buttons

--- src/components/Home/TemplateForm.js
+++ src/components/Home/TemplateForm.js
@@ -1,16 +1,25 @@
 import { useState } from "react"
 
 function TemplateForm() {
-    const [input, setInput] = useState("")
+    const [input, setInput] = useState({message: "", templateName: "" })
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        // TODO: send input to database
+        console.log(input)
+    }
+
+    const handleChange = ({ target }) => {
+        setInput({ ...input, [target.name]: target.value });
         console.log(input)
     }
 
   return (
     <form onSubmit={handleSubmit}>
-        <input onChange={(e) => {setInput(e.target.value); console.log(input)}}></input>
+        <button name="templateName" type="button" onClick={handleChange} value="1">1</button>
+        <button name="templateName" type="button" onClick={handleChange} value="2">2</button>
+        <button name="templateName" type="button" onClick={handleChange} value="3">3</button>
+        <input name="message" onChange={handleChange}></input>
         <button type="submit">submit</button>
     </form>
   )
